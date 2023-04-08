36b6c8396adcbd0773da58f98dfb22ff369861e1
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-04-07 19:24:13 -0500

    task 0

--- markdown2html.py
+++ markdown2html.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python3
+
+"""
+Script markdown2html.py that takes an argument 2 strings:
+First argument is the name of the Markdown file
+Second argument is the output file name
+"""
+
 import sys
 import os
 import markdown
